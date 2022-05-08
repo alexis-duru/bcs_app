9e9e8eaa0c45846da4bb9d45aa27a0da13519c9c
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-05-08 21:08:32 +0200

    creation d'une fonction permettant de hash le password

---
+++ api/src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface {
+
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher){
+
+        $this->hasher = $hasher;
+    }
+
+    public static function getSubscribedEvents() 
+    {
+        return 
+        [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(ViewEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        // $response = new Response();
+        if($user instanceof User && $method === "POST")  {
+            $hash = $this->hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+        }
+        // dd($user);
+    }
+}
+
+?>
+
+// UserPasswordHasherInterface
+
+// hashPassword() 
