8407a5fa903154fe98015382ea00b7a6e77bf656
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-05-23 23:03:02 +0200

    [create] scss login form

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -5,7 +5,6 @@ const LoginPage = (props) => {
         <>
           <div className="globalPage">
             <div className="leftSideBar">
-
             </div>
             <div className="globalPageWrapper">
                 <div className="globalPageHeader">
@@ -13,6 +12,20 @@ const LoginPage = (props) => {
                     </div>
                 </div>
                 <div className="globalPageWrapperCards">
+                <form>
+                    <div className="form_overlay"></div>
+                    <div className="form-group">
+                        <label htmlFor="_username">Email adress</label>
+                        <input type="email"  placeholder="email adress" name="username" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="password" name="password" id="password" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                       <button type="submit" className="submitBtn">Login</button>
+                    </div>
+                </form>
                 <div className="globalPageWrapperCards_overlay"></div>
                 </div>
                 <div className="globalFullPaginationContainer">
