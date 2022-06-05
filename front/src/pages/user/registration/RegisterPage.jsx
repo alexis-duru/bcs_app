077a174ba585c43c59147a393a2b17006120f0c2
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2022-06-05 18:46:48 +0200

    [create] sign in form

--- front/src/pages/user/registration/RegisterPage.jsx
+++ front/src/pages/user/registration/RegisterPage.jsx
@@ -1,10 +1,89 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Field from '../../../components/forms/field';
+import usersAPI from "../../../services/usersAPI";
 
-const registerPage = (props) => {
+const RegisterPage = (props) => {
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        passwordConfirm: "",
+
+    });
+
+    const [error, setErrors] = useState({
+        email: "",
+        password: "",
+        passwordConfirm: "",
+
+    });
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const response = await usersAPI.createUser()
+        } catch (error) {
+            console.log(error.response)
+            const {violations} = error.response.data
+        }
+        console.log(user)
+    }
+
+    const handleChange = ({currentTarget}) => {
+        const {name, value} = currentTarget;
+        setUser({...user, [name]: value})
+    }
+
     return (  
 
-        <h1>Register Page</h1>
+        <div>
+            <h1>Register page</h1>
+            <h1>Register page</h1>
+            <h1>Register page</h1>
+            <h1>Register page</h1>
+            <h1>Register page</h1>
+            <form onSubmit={handleSubmit}>
+                <Field
+                    name= "email"
+                    label= "email"
+                    type="email"
+                    placeholder="email"
+                    error={error.email}
+                    value={user.email}
+                    onChange={handleChange}
+                />
+                 <Field
+                    name= "password"
+                    label= "password"
+                    type="password"
+                    placeholder="password"
+                    error={error.password}
+                    value={user.password}
+                    onChange={handleChange}
+                />
+                 <Field
+                    name= "passwordConfirm"
+                    label= "passwordConfirm"
+                    type="password"
+                    placeholder="passwordConfirm"
+                    error={error.passwordConfirm}
+                    value={user.passwordConfirm}
+                    onChange={handleChange}
+                />
+                <div>
+                    <button type="submit">
+                            Sign In
+                    </button>
+                </div>
+                <Link to='/login'>
+                    I have an already account
+                </Link>
+            </form>
+        </div>
     );
 }
  
-export default registerPage;
+export default RegisterPage;
---
+++ front/src/services/usersAPI.js
@@ -0,0 +1,35 @@
+// import axios from "axios";
+import API from "./api";
+
+function findAllUsers() {
+    return API 
+        .get("/users")
+        .then(response => response.data['hydra:member'])
+};
+
+const findOneUser = async (id) => {
+    return API
+        .get(`/users/${id}`)
+        .then(response => response.data)
+};
+
+const createUser = (credentials) => {
+    return API 
+        .post('/users', credentials)
+        .then(response => response.data)
+}
+
+
+function deleteUser(id) {
+    return API
+    .delete(`/users/${id}`);
+}
+
+const exportAPI =  {
+    findAllUsers,
+    delete: deleteUser,
+    findOneUser,
+    createUser,
+};
+
+export default exportAPI;
